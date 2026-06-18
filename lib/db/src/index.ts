ef89b300f70558ce9752a01c70b6170637359644
Moses Juma <mosesjumawafula@gmail.com>
2026-06-18 23:29:10 +0300

    Spanish

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -19,4 +19,4 @@ if (!process.env.DATABASE_URL) {
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
-export * from "./schema";
+export * from "./schema"; 
