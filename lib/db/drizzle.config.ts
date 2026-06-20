740bd94d9061c61466bc110da1bdbe946e56a356
Moses Juma <mosesjumawafula@gmail.com>
2026-06-20 15:31:45 +0300

    Updated with lubisiaTech

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "drizzle-kit";
-import path from "path";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+	throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
 export default defineConfig({
-  schema: path.join(__dirname, "./src/schema/index.ts"),
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
+	schema: "./src/schema/index.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: process.env.DATABASE_URL,
+	},
 });
